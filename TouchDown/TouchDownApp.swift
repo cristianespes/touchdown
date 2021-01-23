85a46b445117d8c13ab57e3049f965b13fed27f9
Cristian Espes <christian_zgza@hotmail.com>
2021-01-23 12:46:17 +0100

    First commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Cristian Espes on 23/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
